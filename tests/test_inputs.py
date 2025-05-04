724d077b19f497fbe41716a530df190f796d3309
Johannes Roos <jhnnsrs@gmail.com>
2025-05-04 19:39:02 +0200

    feat: Refactor port tests to separate integer and float port validations

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -3,6 +3,11 @@
 from fluss_next.api.schema import PortInput, PortKind
 
 
-def test_port() -> None:
-    """Test the NodeException scalar."""
+def test_int_port() -> None:
+    """Test the Input Port."""
     PortInput(key="1234", kind=PortKind.INT, nullable=False)
+
+
+def test_float_port() -> None:
+    """Test the Float Port."""
+    PortInput(key="1234", kind=PortKind.FLOAT, nullable=False)
