92d2150e291332e2c6d358bd53585c6c0ede80bb
Johannes Roos <jhnnsrs@gmail.com>
2026-06-25 17:36:00 +0200

    feat!: adapted to now api

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -1,13 +1,13 @@
 """Tests for the inputs module."""
 
-from fluss_next.api.schema import PortInput, PortKind
+from fluss_next.api.schema import ArgPortInput, PortKind
 
 
 def test_int_port() -> None:
     """Test the Input Port."""
-    PortInput(key="1234", kind=PortKind.INT, nullable=False)
+    ArgPortInput(key="1234", kind=PortKind.INT, nullable=False)
 
 
 def test_float_port() -> None:
     """Test the Float Port."""
-    PortInput(key="1234", kind=PortKind.FLOAT, nullable=False)
+    ArgPortInput(key="1234", kind=PortKind.FLOAT, nullable=False)
